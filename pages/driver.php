8e0f27cac42d33450555a9c13442fe2a00318846
Robert Couldridge <robert.couldridge@bt.com>
2024-11-29 14:39:39 +0000

    driver details begining to implement

--- pages/driver.php
+++ pages/driver.php
@@ -7,7 +7,23 @@ $drivers = $Driver->getDriverById($driver_id);
 <body id="driver">
 
 <?php foreach($drivers as $driver) {?>
-    <h2><?php echo $driver['driver_name'];?></h2>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="./images/<?php echo $driver['driver_image_full_length'];?>">
+            </div>
+            <div class="col-md-6">
+                <h2><?php echo $driver['driver_name'];?></h2>
+                <ul class="driver-stats">
+                    <li>Born: <?php echo $driver['born'];?></li>
+                    <li>Weight: <?php echo $driver['weight'];?>kg</li>
+                    <li><i class="bi bi-rulers"></i>Height: <?php echo $driver['height'];?>m</li>
+                    <li>Country: <?php echo $driver['country'];?></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 <?php } ?>
 
 </body> 
