e79dd0677e55fbb822724b1f5ab3fe1da095ccb2
Robert Couldridge <robert.couldridge@bt.com>
2024-12-15 17:29:35 +0000

    order form now checks if the quantity is available of a part in the stores

--- pages/part.php
+++ pages/part.php
@@ -1,15 +1,53 @@
 <?php
-$part_id = $_GET['id'];
-$Part = new Part($Conn);
-$parts = $Part->getPartById($part_id);
-$Race = new Race($Conn);
-$races = $Race->getRacesByDate("2024-08-01");
+    $part_id = $_GET['id'];
+    $Part = new Part($Conn);
+    $parts = $Part->getPartById($part_id);
+    $Race = new Race($Conn);
+    $races = $Race->getRacesByDate("2024-08-01");
 ?>
-
 <body id="part">
-
 <?php foreach($parts as $part) {?>
     <div class="container">
+        <?php
+            if($_POST['add-to-order']){
+                $destination = $_POST["races"];
+                $quantity = $_POST["quantity"];
+
+                if (!isset($_SESSION['cart'])) {
+                    $_SESSION['cart'] = [];
+                }
+                // add to cart and check for errors
+                $error = "";
+                if (isset($_SESSION['cart'][$part_id])) {
+                    if( $_SESSION['cart'][$part_id] + $quantity > $part['quantity_in_stores']){
+                        $error = "Not enough {$part['part_name']}s in stores";
+                    } else {
+                    $_SESSION['cart'][$part_id] += $quantity;
+                    }
+                } else {
+                    if($quantity > $part['quantity_in_stores']){
+                        $error = "Not enough {$part['part_name']}s in stores";
+                    } else {
+                    $_SESSION['cart'][$part_id] = $quantity;
+                    }
+                }
+                if($error){
+                    ?>
+                        <div class="alert alert-danger alert-dismissible">
+                            <?php echo $error; ?>
+                            <button class="alert-close" data-dismiss="alert">X</button>
+                        </div>
+                    <?php
+                } else {
+                    ?>
+                        <div class="alert alert-success alert-dismissible">
+                            Parts added to order!
+                            <button class="alert-close" data-dismiss="alert">X</button>
+                        </div>
+                    <?php
+                }
+            }
+            ?>
         <div class="row">
             <div class="col-md-6">
                 <img class="part-image" src="./images/<?php echo $part['part_image'];?>">
@@ -30,14 +68,20 @@ $races = $Race->getRacesByDate("2024-08-01");
                         </tr>
                     </table>
                 </div>
-                <div class="race-selection">
-                    <label for="races">Select Destination:</label>
+                <br><br>
+                <form class="part-order-form" id="part-order-form" method="post" action="">
+                    <label for="races">Destination:</label>
                     <select name="races" id="races">
                         <?php foreach($races as $race) {?>
                         <option value=<?php echo $race['circuit_name'];?>><?php echo $race['circuit_name'];?></option>
                         <?php } ?>
                     </select>
-                </div>
+                    <br><br>
+                    <label for="quantity">Quantity:</label>
+                    <input type="number" id="quantity" name="quantity">
+                    <br><br>
+                    <button type="submit" name="add-to-order" value="1">Add to Order</button>
+                </form>
             </div>
         </div>
     </div>
